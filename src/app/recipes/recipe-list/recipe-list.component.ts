c0e161e6e50c1ae3fb24bcc9743d787c38d07524
Ilias Dinia <iliasd752@gmail.com>
2021-05-19 16:17:05 +0200

    course update

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A test recipe', 'This is simply a test', 'https://pixnio.com/free-images/2017/03/25/2017-03-25-09-34-43-725x483.jpg')
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
